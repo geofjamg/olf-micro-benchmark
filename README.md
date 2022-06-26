0d8251b817a1dcbe14dc4f2cf6b4b52cd52f3860
Geoffroy Jamgotchian <geoffroy.jamgotchian@gmail.com>
2022-06-26 21:10:40 +0200

    Wip

--- README.md
+++ README.md
@@ -1,22 +1,11 @@
 ```
-Benchmark                                                                             Mode  Cnt     Score     Error  Units
-EquationTermBenchmark.objectModelShuffled                                             avgt    5  3620,917 ± 759,494  us/op
-EquationTermBenchmark.objectModelSortedByBranch                                       avgt    5  1288,188 ±  21,172  us/op
-EquationTermBenchmark.staticMethodSortedByBranch                                      avgt    5   436,720 ±   9,162  us/op
-EquationTermBenchmark.staticMethodSortedByBranchWithCurrentCalculatedFromActivePower  avgt    5   237,936 ±   4,163  us/op
-EquationTermBenchmark.staticMethodSortedByTermType                                    avgt    5   556,821 ±  57,716  us/op
-
-Benchmark                                                                             Mode  Cnt     Score     Error  Units
-EquationTermBenchmark.objectModelShuffled                                             avgt    5  3407,573 ± 651,486  us/op
-EquationTermBenchmark.objectModelSortedByBranch                                       avgt    5  1327,676 ±  60,141  us/op
-EquationTermBenchmark.staticMethodSortedByBranch                                      avgt    5   697,932 ± 127,784  us/op
-EquationTermBenchmark.staticMethodSortedByBranchWithCurrentCalculatedFromActivePower  avgt    5   326,041 ±   2,786  us/op
-EquationTermBenchmark.staticMethodSortedByTermType                                    avgt    5   784,715 ±  79,234  us/op
-
-Benchmark                                                                             Mode  Cnt     Score    Error  Units
-EquationTermBenchmark.objectModelShuffled                                             avgt    5  3086,862 ± 37,365  us/op
-EquationTermBenchmark.objectModelSortedByBranch                                       avgt    5  1223,606 ± 54,797  us/op
-EquationTermBenchmark.staticMethodSortedByBranch                                      avgt    5   614,888 ±  1,033  us/op
-EquationTermBenchmark.staticMethodSortedByBranchWithCurrentCalculatedFromActivePower  avgt    5   239,454 ±  0,393  us/op
-EquationTermBenchmark.staticMethodSortedByTermType                                    avgt    5   711,170 ± 76,753  us/op
+Benchmark                                                                                                        Mode  Cnt     Score      Error  Units
+EquationTermBenchmark.objectModelShuffled                                                                        avgt    5  3269,062 ±   15,702  us/op
+EquationTermBenchmark.objectModelSortedByBranch                                                                  avgt    5  1612,496 ±  325,020  us/op
+EquationTermBenchmark.objectModelSortedByBranchAndStoreResults                                                   avgt    5  2434,113 ± 1003,407  us/op
+EquationTermBenchmark.staticMethodSortedByBranch                                                                 avgt    5   592,683 ±   38,069  us/op
+EquationTermBenchmark.staticMethodSortedByBranchWithCurrentCalculatedFromActivePower                             avgt    5   224,712 ±    1,085  us/op
+EquationTermBenchmark.staticMethodSortedByBranchWithCurrentCalculatedFromActivePowerAndStoreResult               avgt    5   209,735 ±    0,974  us/op
+EquationTermBenchmark.staticMethodSortedByBranchWithCurrentCalculatedFromActivePowerAndStoreResultOnlyOneCosSin  avgt    5   220,861 ±    1,090  us/op
+EquationTermBenchmark.staticMethodSortedByTermType                                                               avgt    5   697,439 ±   15,817  us/op
 ```
--- src/main/java/fr/jamgotchian/olf/EquationTermBenchmark.java
+++ src/main/java/fr/jamgotchian/olf/EquationTermBenchmark.java
@@ -1,15 +1,20 @@
 package fr.jamgotchian.olf;
 
 import com.powsybl.openloadflow.ac.equations.*;
+import net.jafama.DoubleWrapper;
 import net.jafama.FastMath;
 import org.openjdk.jmh.annotations.*;
 
 import java.util.concurrent.TimeUnit;
 
+import static com.powsybl.openloadflow.network.PiModel.A2;
+
 @Fork(1)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 public class EquationTermBenchmark {
+
+    private static final double SQRT_3 = FastMath.sqrt(3);
 
     @Benchmark
     public void objectModelSortedByBranch(EquationTermState state) {
@@ -47,6 +52,21 @@ public class EquationTermBenchmark {
         }
     }
 
+    private static void calculateCosSinTheta(int branchCount, double[] ksi, int[] branchBus1Num, int[] branchBus2Num, double[] ph,
+                                             double[] a1, double[] sinTheta1, double[] cosTheta1, double[] sinTheta2, double[] cosTheta2) {
+        var w = new DoubleWrapper();
+        for (int num = 0; num < branchCount; num++) {
+            int bus1Num = branchBus1Num[num];
+            int bus2Num = branchBus2Num[num];
+            double theta1 = AbstractClosedBranchAcFlowEquationTerm.theta1(ksi[num], ph[bus1Num], a1[num], ph[bus2Num]);
+            double theta2 = AbstractClosedBranchAcFlowEquationTerm.theta2(ksi[num], ph[bus1Num], a1[num], ph[bus2Num]);
+            sinTheta1[num] = FastMath.sinAndCos(theta1, w);
+            cosTheta1[num] = w.value;
+            sinTheta2[num] = FastMath.sinAndCos(theta2, w);
+            cosTheta2[num] = w.value;
+        }
+    }
+
     @Benchmark
     public void staticMethodSortedByBranch(EquationTermState state) {
         int[] branchBus1Num = state.getBranchBus1Num();
@@ -64,6 +84,11 @@ public class EquationTermBenchmark {
         double[] v = state.getV();
         double[] ph = state.getPh();
         int branchCount = state.getBranchCount();
+        double[] sinTheta1 = new double[state.getBranchCount()];
+        double[] cosTheta1 = new double[state.getBranchCount()];
+        double[] sinTheta2 = new double[state.getBranchCount()];
+        double[] cosTheta2 = new double[state.getBranchCount()];
+        calculateCosSinTheta(branchCount, ksi, branchBus1Num, branchBus2Num, ph, a1, sinTheta1, cosTheta1, sinTheta2, cosTheta2);
         double[] p1 = new double[branchCount];
         double[] q1 = new double[branchCount];
         double[] i1 = new double[branchCount];
@@ -73,11 +98,11 @@ public class EquationTermBenchmark {
         for (int num = 0; num < branchCount; num++) {
             int bus1Num = branchBus1Num[num];
             int bus2Num = branchBus2Num[num];
-            p1[num] = ClosedBranchSide1ActiveFlowEquationTerm.p1(y[num], ksi[num], sinKsi[num], g1[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            q1[num] = ClosedBranchSide1ReactiveFlowEquationTerm.q1(y[num], ksi[num], cosKsi[num], b1[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
+            p1[num] = ClosedBranchSide1ActiveFlowEquationTerm.p1(y[num], sinKsi[num], g1[num], v[bus1Num], r1[num], v[bus2Num], sinTheta1[num]);
+            q1[num] = ClosedBranchSide1ReactiveFlowEquationTerm.q1(y[num], cosKsi[num], b1[num], v[bus1Num], r1[num], v[bus2Num], cosTheta1[num]);
             i1[num] = ClosedBranchSide1CurrentMagnitudeEquationTerm.i1(y[num], ksi[num], g1[num], b1[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            p2[num] = ClosedBranchSide2ActiveFlowEquationTerm.p2(y[num], ksi[num],sinKsi[num], g2[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            q2[num] = ClosedBranchSide2ReactiveFlowEquationTerm.q2(y[num], ksi[num], cosKsi[num], b2[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
+            p2[num] = ClosedBranchSide2ActiveFlowEquationTerm.p2(y[num], sinKsi[num], g2[num], v[bus1Num], r1[num], v[bus2Num], sinTheta2[num]);
+            q2[num] = ClosedBranchSide2ReactiveFlowEquationTerm.q2(y[num], cosKsi[num], b2[num], v[bus1Num], r1[num], v[bus2Num], cosTheta2[num]);
             i2[num] = ClosedBranchSide2CurrentMagnitudeEquationTerm.i2(y[num], ksi[num], g2[num], b2[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
         }
     }
@@ -99,6 +124,11 @@ public class EquationTermBenchmark {
         double[] v = state.getV();
         double[] ph = state.getPh();
         int branchCount = state.getBranchCount();
+        double[] sinTheta1 = new double[state.getBranchCount()];
+        double[] cosTheta1 = new double[state.getBranchCount()];
+        double[] sinTheta2 = new double[state.getBranchCount()];
+        double[] cosTheta2 = new double[state.getBranchCount()];
+        calculateCosSinTheta(branchCount, ksi, branchBus1Num, branchBus2Num, ph, a1, sinTheta1, cosTheta1, sinTheta2, cosTheta2);
         double[] p1 = new double[branchCount];
         double[] q1 = new double[branchCount];
         double[] i1 = new double[branchCount];
@@ -108,12 +138,12 @@ public class EquationTermBenchmark {
         for (int num = 0; num < branchCount; num++) {
             int bus1Num = branchBus1Num[num];
             int bus2Num = branchBus2Num[num];
-            p1[num] = ClosedBranchSide1ActiveFlowEquationTerm.p1(y[num], ksi[num], sinKsi[num], g1[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
+            p1[num] = ClosedBranchSide1ActiveFlowEquationTerm.p1(y[num], sinKsi[num], g1[num], v[bus1Num], r1[num], v[bus2Num], sinTheta1[num]);
         }
         for (int num = 0; num < branchCount; num++) {
             int bus1Num = branchBus1Num[num];
             int bus2Num = branchBus2Num[num];
-            q1[num] = ClosedBranchSide1ReactiveFlowEquationTerm.q1(y[num], ksi[num], cosKsi[num], b1[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
+            q1[num] = ClosedBranchSide1ReactiveFlowEquationTerm.q1(y[num], cosKsi[num], b1[num], v[bus1Num], r1[num], v[bus2Num], cosTheta1[num]);
         }
         for (int num = 0; num < branchCount; num++) {
             int bus1Num = branchBus1Num[num];
@@ -123,12 +153,12 @@ public class EquationTermBenchmark {
         for (int num = 0; num < branchCount; num++) {
             int bus1Num = branchBus1Num[num];
             int bus2Num = branchBus2Num[num];
-            p2[num] = ClosedBranchSide2ActiveFlowEquationTerm.p2(y[num], ksi[num], sinKsi[num], g2[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
+            p2[num] = ClosedBranchSide2ActiveFlowEquationTerm.p2(y[num], sinKsi[num], g2[num], v[bus1Num], r1[num], v[bus2Num], sinTheta2[num]);
         }
         for (int num = 0; num < branchCount; num++) {
             int bus1Num = branchBus1Num[num];
             int bus2Num = branchBus2Num[num];
-            q2[num] = ClosedBranchSide2ReactiveFlowEquationTerm.q2(y[num], ksi[num], cosKsi[num], b2[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
+            q2[num] = ClosedBranchSide2ReactiveFlowEquationTerm.q2(y[num], cosKsi[num], b2[num], v[bus1Num], r1[num], v[bus2Num], cosTheta2[num]);
         }
         for (int num = 0; num < branchCount; num++) {
             int bus1Num = branchBus1Num[num];
@@ -154,6 +184,11 @@ public class EquationTermBenchmark {
         double[] v = state.getV();
         double[] ph = state.getPh();
         int branchCount = state.getBranchCount();
+        double[] sinTheta1 = new double[state.getBranchCount()];
+        double[] cosTheta1 = new double[state.getBranchCount()];
+        double[] sinTheta2 = new double[state.getBranchCount()];
+        double[] cosTheta2 = new double[state.getBranchCount()];
+        calculateCosSinTheta(branchCount, ksi, branchBus1Num, branchBus2Num, ph, a1, sinTheta1, cosTheta1, sinTheta2, cosTheta2);
         double[] p1 = new double[branchCount];
         double[] q1 = new double[branchCount];
         double[] i1 = new double[branchCount];
@@ -163,10 +198,10 @@ public class EquationTermBenchmark {
         for (int num = 0; num < branchCount; num++) {
             int bus1Num = branchBus1Num[num];
             int bus2Num = branchBus2Num[num];
-            p1[num] = ClosedBranchSide1ActiveFlowEquationTerm.p1(y[num], ksi[num], sinKsi[num], g1[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            q1[num] = ClosedBranchSide1ReactiveFlowEquationTerm.q1(y[num], ksi[num], cosKsi[num], b1[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            p2[num] = ClosedBranchSide2ActiveFlowEquationTerm.p2(y[num], ksi[num], sinKsi[num], g2[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            q2[num] = ClosedBranchSide2ReactiveFlowEquationTerm.q2(y[num], ksi[num], cosKsi[num], b2[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
+            p1[num] = ClosedBranchSide1ActiveFlowEquationTerm.p1(y[num], sinKsi[num], g1[num], v[bus1Num], r1[num], v[bus2Num], sinTheta1[num]);
+            q1[num] = ClosedBranchSide1ReactiveFlowEquationTerm.q1(y[num], cosKsi[num], b1[num], v[bus1Num], r1[num], v[bus2Num], cosTheta1[num]);
+            p2[num] = ClosedBranchSide2ActiveFlowEquationTerm.p2(y[num], sinKsi[num], g2[num], v[bus1Num], r1[num], v[bus2Num], sinTheta2[num]);
+            q2[num] = ClosedBranchSide2ReactiveFlowEquationTerm.q2(y[num], cosKsi[num], b2[num], v[bus1Num], r1[num], v[bus2Num], cosTheta2[num]);
             i1[num] = FastMath.hypot(p1[num], q1[num]) / (v[bus1Num] * FastMath.sqrt(3) / 1000);
             i2[num] = FastMath.hypot(p2[num], q2[num]) / (v[bus2Num] * FastMath.sqrt(3) / 1000);
         }
@@ -192,15 +227,91 @@ public class EquationTermBenchmark {
         double[] p = new double[state.getBusCount()];
         double[] q = new double[state.getBusCount()];
         double[] i = new double[state.getBusCount()];
+        double[] sinTheta1 = new double[state.getBranchCount()];
+        double[] cosTheta1 = new double[state.getBranchCount()];
+        double[] sinTheta2 = new double[state.getBranchCount()];
+        double[] cosTheta2 = new double[state.getBranchCount()];
+        calculateCosSinTheta(branchCount, ksi, branchBus1Num, branchBus2Num, ph, a1, sinTheta1, cosTheta1, sinTheta2, cosTheta2);
         for (int num = 0; num < branchCount; num++) {
             int bus1Num = branchBus1Num[num];
             int bus2Num = branchBus2Num[num];
-            double p1 = ClosedBranchSide1ActiveFlowEquationTerm.p1(y[num], ksi[num], sinKsi[num], g1[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            double q1 = ClosedBranchSide1ReactiveFlowEquationTerm.q1(y[num], ksi[num], cosKsi[num], b1[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            double p2 = ClosedBranchSide2ActiveFlowEquationTerm.p2(y[num], ksi[num], sinKsi[num], g2[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            double q2 = ClosedBranchSide2ReactiveFlowEquationTerm.q2(y[num], ksi[num], cosKsi[num], b2[num], v[bus1Num], ph[bus1Num], r1[num], a1[num], v[bus2Num], ph[bus2Num]);
-            double i1 = FastMath.hypot(p1, q1) / (v[bus1Num] * FastMath.sqrt(3) / 1000);
-            double i2 = FastMath.hypot(p2, q2) / (v[bus2Num] * FastMath.sqrt(3) / 1000);
+            double p1 = ClosedBranchSide1ActiveFlowEquationTerm.p1(y[num], sinKsi[num], g1[num], v[bus1Num], r1[num], v[bus2Num], sinTheta1[num]);
+            double q1 = ClosedBranchSide1ReactiveFlowEquationTerm.q1(y[num], cosKsi[num], b1[num], v[bus1Num], r1[num], v[bus2Num], cosTheta1[num]);
+            double p2 = ClosedBranchSide2ActiveFlowEquationTerm.p2(y[num], sinKsi[num], g2[num], v[bus1Num], r1[num], v[bus2Num], sinTheta2[num]);
+            double q2 = ClosedBranchSide2ReactiveFlowEquationTerm.q2(y[num], cosKsi[num], b2[num], v[bus1Num], r1[num], v[bus2Num], cosTheta2[num]);
+            double i1 = FastMath.hypot(p1, q1) / (v[bus1Num] * SQRT_3 / 1000);
+            double i2 = FastMath.hypot(p2, q2) / (v[bus2Num] * SQRT_3 / 1000);
+            p[bus1Num] += p1;
+            q[bus1Num] += q1;
+            i[bus1Num] += i1;
+            p[bus2Num] += p2;
+            q[bus2Num] += q2;
+            i[bus2Num] += i2;
+        }
+    }
+
+    // cos(a+b) = cos a cos b - sin a sin b
+    private static double cosAplusB(double cosA, double cosB, double sinA, double sinB) {
+        return cosA * cosB - sinA * sinB;
+    }
+
+    // sin(a+b) = sin a cos b + cos a sin b
+    private static double sinAplusB(double cosA, double cosB, double sinA, double sinB) {
+        return sinA * cosB + cosA * sinB;
+    }
+
+    private static double theta0(double ph1, double a1, double ph2) {
+        return a1 - A2 + ph1 - ph2;
+    }
+
+    @Benchmark
+    public void staticMethodSortedByBranchWithCurrentCalculatedFromActivePowerAndStoreResultOnlyOneCosSin(EquationTermState state) {
+        int[] branchBus1Num = state.getBranchBus1Num();
+        int[] branchBus2Num = state.getBranchBus2Num();
+        double[] y = state.getY();
+        double[] cosKsi = state.getCosKsi();
+        double[] sinKsi = state.getSinKsi();
+        double[] g1 = state.getG1();
+        double[] g2 = state.getG2();
+        double[] b1 = state.getB1();
+        double[] b2 = state.getB2();
+        double[] r1 = state.getR1();
+        double[] a1 = state.getA1();
+        double[] v = state.getV();
+        double[] ph = state.getPh();
+        int branchCount = state.getBranchCount();
+        double[] p = new double[state.getBusCount()];
+        double[] q = new double[state.getBusCount()];
+        double[] i = new double[state.getBusCount()];
+        double[] sinTheta0 = new double[state.getBranchCount()];
+        double[] cosTheta0 = new double[state.getBranchCount()];
+        double[] sinTheta1 = new double[state.getBranchCount()];
+        double[] cosTheta1 = new double[state.getBranchCount()];
+        double[] sinTheta2 = new double[state.getBranchCount()];
+        double[] cosTheta2 = new double[state.getBranchCount()];
+        var w = new DoubleWrapper();
+        for (int num = 0; num < branchCount; num++) {
+            int bus1Num = branchBus1Num[num];
+            int bus2Num = branchBus2Num[num];
+            double theta0 = theta0(ph[bus1Num], a1[num], ph[bus2Num]);
+            sinTheta0[num] = FastMath.sinAndCos(theta0, w);
+            cosTheta0[num] = w.value;
+            // theta1 = ksi - theta0
+            // theta2 = ksi + theta0
+            cosTheta1[num] = cosAplusB(cosKsi[num], cosTheta0[num], sinKsi[num], -sinTheta0[num]);
+            sinTheta1[num] = sinAplusB(cosKsi[num], cosTheta0[num], sinKsi[num], -sinTheta0[num]);
+            cosTheta2[num] = cosAplusB(cosKsi[num], cosTheta0[num], sinKsi[num], sinTheta0[num]);
+            sinTheta2[num] = sinAplusB(cosKsi[num], cosTheta0[num], sinKsi[num], sinTheta0[num]);
+        }
+        for (int num = 0; num < branchCount; num++) {
+            int bus1Num = branchBus1Num[num];
+            int bus2Num = branchBus2Num[num];
+            double p1 = ClosedBranchSide1ActiveFlowEquationTerm.p1(y[num], sinKsi[num], g1[num], v[bus1Num], r1[num], v[bus2Num], sinTheta1[num]);
+            double q1 = ClosedBranchSide1ReactiveFlowEquationTerm.q1(y[num], cosKsi[num], b1[num], v[bus1Num], r1[num], v[bus2Num], cosTheta1[num]);
+            double p2 = ClosedBranchSide2ActiveFlowEquationTerm.p2(y[num], sinKsi[num], g2[num], v[bus1Num], r1[num], v[bus2Num], sinTheta2[num]);
+            double q2 = ClosedBranchSide2ReactiveFlowEquationTerm.q2(y[num], cosKsi[num], b2[num], v[bus1Num], r1[num], v[bus2Num], cosTheta2[num]);
+            double i1 = FastMath.hypot(p1, q1) / (v[bus1Num] * SQRT_3 / 1000);
+            double i2 = FastMath.hypot(p2, q2) / (v[bus2Num] * SQRT_3 / 1000);
             p[bus1Num] += p1;
             q[bus1Num] += q1;
             i[bus1Num] += i1;
